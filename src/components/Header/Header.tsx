9e624918c2b3793d42878b74f7fc8002f1b718b1
Ihor Abovian <abovyan1903@gmail.com>
2026-05-25 19:33:59 -0400

    feat: add Header component with search functionality

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+const Header = () => {
+  return (
+    <header className="header border-b-1 border-gray-300  p-2 flex items-center ">
+      <form className="w-full flex items-center justify-between">
+        <div className="relative inline-block ml-70">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400"
+            aria-hidden="true"
+          >
+            <circle cx="11" cy="11" r="8" />
+            <path d="m21 21-4.3-4.3" />
+          </svg>
+
+          <input
+            type="text"
+            placeholder="Search"
+            className="border rounded border-gray-100 bg-gray-100 py-2 pl-9 pr-4 placeholder:text-gray-400 w-72 focus:outline-none"
+          />
+        </div>
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 text-white rounded mr-70 cursor-pointer"
+        >
+          New Note
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default Header;
