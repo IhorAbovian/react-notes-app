2713a6e915e74ff098f6222f1a4cb4901248ea37
IhorAbovian <abovyan1903@gmail.com>
2026-05-29 19:52:04 -0400

    feat: implement note fetching and display in MainContent component

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,36 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import type { Note } from "../../Pages/MainPage.tsx";
+import { BACKEND_URL } from "../../utils/constants";
 
-// const fetchNote = async () => {
-//   try {
-//     const notesResponse = await fetch(`${BACKEND_URL}/notes`);
+const fetchNote = async (id: string) => {
+  try {
+    const notesResponse = await fetch(`${BACKEND_URL}/notes/${id}`);
 
-//     if (!notesResponse.ok) {
-//       throw notesResponse.status;
-//     }
+    if (!notesResponse.ok) {
+      throw notesResponse.status;
+    }
 
-//     const notes = await notesResponse.json();
+    const note = await notesResponse.json();
 
-//     return { data: notes as Note[], error: null };
-//   } catch (error) {
-//     console.log("Error!", error);
-//     return { data: [], error };
-//   }
-// };
+    return { data: note as Note, error: null };
+  } catch (error) {
+    console.log("Error!", error);
+    return { data: null, error };
+  }
+};
 
 const MainContent = () => {
-  const [selectedNote] = useState<Note | null>(null);
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
 
   const [searchParams] = useSearchParams();
   const selectedNoteId = searchParams.get("selectedNoteId");
+
+  useEffect(() => {
+    if (!selectedNoteId) {
+      setSelectedNote(null);
+      return;
+    }
+    fetchNote(selectedNoteId).then(({ data }) => {
+      setSelectedNote(data);
+    });
+  }, [selectedNoteId]);
 
   return (
     <main className="flex-1 p-4">
       {!selectedNoteId && <p className="text-gray-400">Please select a note</p>}
 
-      {selectedNoteId && selectedNote && (
-        <p className="text-gray-400">Note content...</p>
+      {selectedNote && (
+        <div>
+          <h1 className="text-2xl font-bold mb-2">{selectedNote.title}</h1>
+          <p className="text-gray-700">{selectedNote.body}</p>
+        </div>
       )}
     </main>
   );
